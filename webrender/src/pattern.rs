ac10c6b21792a537fa73231948b3c2bd17e539dc
Sandor Molnar <smolnar@mozilla.com>
2026-02-16 14:30:23 +0200

    Revert "Bug 2016067 - Move the box-shadow pattern into the pattern module. r=gw" for causing build bustages
    This reverts commit 5c9ac01fb3915d8a78804e47ec4f4ad39e272990.
    
    Revert "Bug 2016067 - Begin moving the pattern code into a sub-directory. r=gw"
    
    This reverts commit 8ba333e2e9b0f06a5cebd8562d3203b405ce013d.

--- webrender/src/pattern.rs
+++ webrender/src/pattern.rs
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pub mod gradient;
-pub mod box_shadow;
-
 use api::{ColorF, units::DeviceRect};
 
 use crate::clip::{ClipIntern, ClipStore};
