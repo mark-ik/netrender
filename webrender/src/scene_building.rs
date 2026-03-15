83b4309eb562fb88a242f9b57d1db4f97445570d
Serban Stanca <sstanca@mozilla.com>
2026-03-15 06:19:06 +0200

    Revert "Bug 1957175 - fix for wpt-meta." for causing wpt failures.
    This reverts commit 6fa2e3d2793d640a28e368e910d068a813aa3efd.
    
    This reverts commit 114c74044ba1326a104ad0911ecc36e48830ed1f.
    
    This reverts commit 38283a6f07c8f0198b7dac2c7835816294192a56.
    
    This reverts commit 0210dcf5582a7df15fa300e4eece1af7fdd3e1a8.
    
    This reverts commit a3db7babe56928859f509c3a8fd47518814f0915.
    
    This reverts commit bd52440d672ec9cd03e88b771aa9fec8aa51bdae.

--- webrender/src/scene_building.rs
+++ webrender/src/scene_building.rs
@@ -1745,7 +1745,6 @@ impl<'a> SceneBuilder<'a> {
                     &mut aa_mask,
                     &layout.clip_rect,
                     info.gradient.radius,
-                    info.gradient.start_offset,
                     info.gradient.end_offset,
                     info.gradient.extend_mode,
                     &stops,
